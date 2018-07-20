6c3684c09299652bb0a57dbd2ae8221338ac5afd
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-07-20 16:24:29 -0300

    rename for ApiProxy

--- src/test/java/com/muralis/apiproxy/ApiProxyApplicationTests.java
+++ src/test/java/com/muralis/apiproxy/ApiProxyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.muralis.apiwrapper;
+package com.muralis.apiproxy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApiWrapperApplicationTests {
+public class ApiProxyApplicationTests {
 
 	@Test
 	public void contextLoads() {
