be7045f788ad6417c0b076c12245a85e0224f13a
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-07-23 14:39:42 -0300

    add .propertie de configuração externa em ./config/config.properties

--- src/main/java/com/muralis/apiproxy/ApiProxyApplication.java
+++ src/main/java/com/muralis/apiproxy/ApiProxyApplication.java
@@ -2,11 +2,14 @@ package com.muralis.apiproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySource("file:./config/config.properties")
 public class ApiProxyApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(ApiProxyApplication.class, args);
 	}
 }
